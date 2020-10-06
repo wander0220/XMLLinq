686c21206264e53eec99e24e2b4e16d92fac0ab6
wander0220 <s2018s26@e-mirim.hs.kr>
2020-10-06 10:20:56 +0900

    초기 커밋.

---
+++ XMLLinq/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace XMLLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "http://www.kma.go.kr/wid/queryDFSRSS.jsp?zone=1150061500";
+            XElement xElement = XElement.Load(url);  // 특정 URL에서 XML을 가져온다.
+            Console.WriteLine(xElement);
+        }
+    }
+}
